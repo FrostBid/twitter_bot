d32c26df9fc5bc7ad68c2071aec91e00b1043a11
FrostBid <83489811+FrostBid@users.noreply.github.com>
2021-10-09 15:00:56 +0800

    added tweeting schedule

--- twitter_bot/twitter_bot.py
+++ twitter_bot/twitter_bot.py
@@ -3,6 +3,9 @@ import tweepy
 import logging
 import os
 from secret import *
+import json
+import tweepy
+from time import sleep
 
 auth = tweepy.OAuthHandler(apikey, apisecret)
 api = tweepy.API(auth)
@@ -12,5 +15,13 @@ try:
     print("Authentication OK")
 except:
     print("Error during authentication")
+
+
+days = 2994
+while True:
+    api.update_status(f"It has been {days} days since the release of GTA 5, still no release of GTA 6...\n#GTA6 #GTA #GTA5")
+    days += 1
+    sleep(86400)
+    print('Tweet successful')
 
 
