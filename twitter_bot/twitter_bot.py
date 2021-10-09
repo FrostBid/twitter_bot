3904dffeee180fd22ae314481054681ded7b3b6d
FrostBid <83489811+FrostBid@users.noreply.github.com>
2021-10-09 11:45:25 +0800

    added auth

--- twitter_bot/twitter_bot.py
+++ twitter_bot/twitter_bot.py
@@ -1,1 +1,16 @@
 """Main module."""
+import tweepy
+import logging
+import os
+from secret import *
+
+auth = tweepy.OAuthHandler(apikey, apisecret)
+api = tweepy.API(auth)
+auth.set_access_token(accesstoken, accesstokensecret)
+try:
+    api.verify_credentials()
+    print("Authentication OK")
+except:
+    print("Error during authentication")
+
+
